78b2a95c845c3b7c6856fd5b9454f77cb3d0b666
Anas011232 <nulltohero@gmail.com>
2026-06-06 00:33:55 +0600

    6/6/26

---
+++ Find First_and_Last_Position_of_Element_in_Sorted_Array.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+
+        int l=0;
+        int r=nums.size()-1;
+
+        vector<int>result(2,-1);
+
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+
+            if(nums[mid]==target)
+            {
+                result[0]=mid;
+                r=mid-1;
+
+
+            }
+            else if(nums[mid]<target)
+            {
+                l=mid+1;
+            }
+            else
+            {
+                r=mid-1;
+            }
+        }
+
+        l=0;
+        r=nums.size()-1;
+
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+
+            if(nums[mid]==target)
+            {
+                result[1]=mid;
+                l=mid+1;
+
+            }
+            else if(nums[mid]<target)
+            {
+                l=mid+1;
+
+            }
+            else
+            {
+                r=mid-1;
+            }
+        }
+
+        return result;
+
+     
+    }
+};
