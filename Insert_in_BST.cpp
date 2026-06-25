70fc196ceb2230797c8594724b176526992c349c
Anas011232 <nulltohero@gmail.com>
2026-06-25 12:03:24 +0600

    Add initial implementation of Node class for Binary Search Tree

---
+++ Insert_in_BST.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+    int val;
+    Node* left;
+    Node* right;
+
+    Node(int value)
+    {
+        val=value;
+        left=NULL;
+        right=NULL;
+    }
+
+
+};
+
+int main()
+{
+    
+    return 0;
+}
