beef22c92dec7c78f12a0d6a95a7aa7278bc91eb
Anas011232 <nulltohero@gmail.com>
2026-06-25 12:07:43 +0600

    Add inorder traversal function for Binary Search Tree

--- Insert_in_BST.cpp
+++ Insert_in_BST.cpp
@@ -36,6 +36,19 @@ Node* insert(Node* root, int key)
     return root;
 }
 
+void inorder(Node* root)
+{
+    if(root==NULL)
+    {
+        return;
+
+    }
+
+    inorder(root->left);
+    cout<<root->val<<" ";
+    inorder(root->right);
+}
+
 int main()
 {
     
